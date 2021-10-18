7235ef73c69ad2f27e1cde61754c43550a7fd324
Kye Felton <kye.felton@hotmail.com>
2021-10-18 17:53:47 +1100

    refactor app.js and header.js

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,126 +1,59 @@
-import RecomList from "./components/RecomList"
-import ResultBox from "./components/ResultBox"
-import ResultList from "./components/ResultList"
-import axios from "axios"
+import Header from "./components/Header"
+import List from "./components/List"
+import Panel from "./components/Panel"
+import { entitySearch, literalSearch, nameSearch } from './components/API'
 import { useState } from "react"
 import "./App.css"
-import { readString } from "react-papaparse"
-import logo from './logo.png'
-import { BsSearch } from "react-icons/bs";
 
-const App = () => {
 
-    const url = "http://localhost:5820/usyd/query"
-    const auth = { username: "user", password: "usyd" }
-    const headers = { "Accept": "application/json-ld, text/csv" }
+export default function App() {
 
-    const [results, setResults] = useState([])
-    const [entity, setEntity] = useState([])
-    const [query, setQuery] = useState('')
+    const [list, setList] = useState(null)
+    const [panel, setPanel] = useState(null)
 
-    const handleQueryChange = (event) => {
-        setQuery(event.target.value)
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        let ft_sparql = `
-            prefix fts: <tag:stardog:api:search:>
-            prefix ont: <http://www.sydney.edu.au/ont/>
-            prefix owl: <http://www.w3.org/2002/07/owl#>
-                    
-            SELECT ?s (MAX(?score) AS ?score) 
-                (SAMPLE(?name) AS ?name) 
-                (SAMPLE(?highlight) AS ?highlight) 
-                (SAMPLE(?homepage) AS ?homepage)
-                (SAMPLE(?class) AS ?class) 
-                (SAMPLE(?desc) AS ?desc) WHERE {
-            
-                ?s ?p ?result ; 
-                    ont:name ?name ; 
-                    ont:homepage ?homepage ;
-                    a ?class .
-
-                BIND(CONCAT(STR( ?p ), STR( ?highlight )) AS ?highlight ) .
-
-                OPTIONAL { ?s ont:description ?desc . }
-
-                FILTER ( ?class NOT IN ( owl:Thing ) )
-
-                
-                { 
-                    SELECT ?score ?highlight ?result WHERE {
-                        SERVICE fts:textMatch {
-                            [] fts:query "${query}";
-                                fts:score ?score ;
-                                fts:result ?result ;
-                                fts:threshold 1.0;
-                                fts:highlight ?highlight 
-                        } 
-                    } 
-                }
-            } 
-            GROUP BY ?s 
-            ORDER BY desc(?score)
-            LIMIT 50
-        `
-        axios.get(url, { params: { query: ft_sparql }, auth: auth, headers: headers })
-            .then(response => {
-                let csvResults = readString(response.data).data
-                csvResults.shift()
-                csvResults.pop()
-                setResults(csvResults)
-            })
-        let ent_sparql = `
-        prefix stardog: <tag:stardog:api:property:>
-        prefix ont: <http://www.sydney.edu.au/ont/>
-        
-        SELECT ?label ?desc ?link WHERE { 
-            ?uri rdfs:label ?label .
-            (?label ?score) stardog:textMatch "${query}" . 
-            optional { ?uri ont:homepage ?link } .
-            optional { ?uri ont:description ?desc } .
+    async function updateList(query, filter, type) {
+        setList([])
+        setPanel(null)
+        let results = []
+        switch (type) {
+            case 'name':
+                results = await nameSearch(query, filter)
+                break
+            case 'literal':
+                results = await literalSearch(query, filter)
+                break
+            default:
+                results = await nameSearch(query, filter)
+                break
         }
-        order by desc(?score)
-        limit 1
-        `
-        axios.get(url, { params: { query: ent_sparql }, auth: auth, headers: headers })
-            .then(response => {
-                let csvResults = readString(response.data).data
-                csvResults.shift()
-                setEntity(csvResults.shift())
-            })
+        setList(results)
+        if (results[0][0] !== "") {
+            let entity = []
+            entity = await entitySearch(results[0][0])
+            setPanel(entity)
+        }
     }
+
+    async function updatePanel(uri) {
+        setPanel(null)
+        let entity = []
+        entity = await entitySearch(uri)
+        setPanel(entity)
+    }
+
+
 
     return (
         <div className="app" >
             <div className="main" >
-                <header className="header container mb-3">
-                    <div className="row mb-4">
-                        <div className="col-sm-5 col-lg-4 col-xl-3">
-                            <img src={logo} className="img-fluid" alt="Univeristy of Sydney"></img>
-                        </div>
-                        <div className="col-sm-7 col-lg-8 col-xl-9">
-                            <p className="text-end"></p>
-                        </div>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" value={query} onChange={handleQueryChange} />
-                            <button type="button" className="btn btn-primary" onClick={handleSubmit}>
-                                <BsSearch />
-                            </button>
-                        </div>
-                    </form>
-                </header>
-                <RecomList />
+                <Header updateList={updateList}/>
                 <div className="container" >
                     <div className="row" >
                         <div className="col-8" >
-                            <ResultList results={results} />
+                            <List results={list}/>
                         </div>
                         <div className="col-4" >
-                            <ResultBox entity={entity} />
+                            <Panel results={panel} updatePanel={updatePanel}/>
                         </div>
                     </div>
                 </div>
@@ -130,7 +63,5 @@ const App = () => {
                 Please view on a larger screen.
             </div>
         </div>
-    );
+    )
 }
-
-export default App;
---
+++ search-app/src/components/Header.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { BsSearch } from 'react-icons/bs'
+import logo from '../logo.png'
+import Select from 'react-select'
+import { useState } from "react"
+
+
+const FILTERS = [
+    { value: 'none', label: '-- no filter --' },
+    { value: 'Centre', label: 'Centre' },
+    { value: 'Course', label: 'Course' },
+    { value: 'Department', label: 'Department' },
+    { value: 'Generic', label: 'Generic' },
+    { value: 'News', label: 'News' },
+    { value: 'Publication', label: 'Publication' },
+    { value: 'Research', label: 'Research' },
+    { value: 'Staff', label: 'Staff' },
+    { value: 'Unit', label: 'Unit' },
+]
+
+const SEARCH_TYPES = [
+    { value: 'name', label: 'Name Search' },
+    { value: 'literal', label: 'Literal Search' },
+]
+
+
+export default function Header(props) {
+
+    const [filter, setFilter] = useState('')
+    const [query, setQuery] = useState('')
+    const [type, setType] = useState('name')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        props.updateList(query, filter, type)
+    }
+
+    function handleFilterChange(selectedOption) {
+        if (selectedOption.value !== 'none') {
+            setFilter(`FILTER ( ?class IN ( ont:${selectedOption.value} ) )`)
+        }
+        else {
+            setFilter('')
+        }
+    }
+
+    function handleQueryChange(event) {
+        setQuery(event.target.value)
+    }
+
+    function handleTypeChange(selectedOption) {
+        setType( selectedOption.value )
+    }
+
+    return (
+        <header className="header container mb-3">
+            <div className="row mb-4">
+                <div className="col-sm-5 col-lg-4 col-xl-3">
+                    <img src={logo} className="img-fluid" alt="Univeristy of Sydney"></img>
+                </div>
+                <div className="col-sm-3 col-lg-4 col-xl-5">
+                </div>
+                <div className="col-sm-4 col-lg-4 col-xl-4">
+                    <Select options={FILTERS} menuPortalTarget={document.body} styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }} onChange={handleFilterChange} />
+                    <p></p>
+                    <Select options={SEARCH_TYPES} menuPortalTarget={document.body} styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }} onChange={handleTypeChange} />
+                </div>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" value={query} onChange={handleQueryChange} />
+                    <button type="submit" className="btn btn-primary">
+                        <BsSearch />
+                    </button>
+                </div>
+            </form>
+        </header>
+    )
+}
