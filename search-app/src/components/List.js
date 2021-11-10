608d1e30456c6bbccb39a6f611e49de40f2008df
Kye Felton <kye.felton@hotmail.com>
2021-11-10 19:04:35 +1100

    remove text-matching score

--- search-app/src/components/List.js
+++ search-app/src/components/List.js
@@ -2,10 +2,9 @@ import React from "react"
 
 export default function List(props) {
 
-    const formatter = new Intl.NumberFormat('en-US', {
-        minimumFractionDigits: 2,      
-        maximumFractionDigits: 2,
-    })
+    function handleEntityClick(uri) {
+        props.updatePanel(uri)
+    }
 
     if (props.results === null) {
         return (
@@ -27,12 +26,11 @@ export default function List(props) {
             <ul className="list-group list-group-flush">
                 {props.results.map((result, index) =>
                     <li id={index} className="list-group-item list-group-item-action text-break">
-                        <a href={result[6]} className="result-title" target="_blank" rel="noreferrer">
+                        <a className="result-title" href="#header" onClick={() => handleEntityClick(result[0])}>
                             {result[2] === "" ? result[3] : result[2]}
                         </a>
-                        <p className="result-score">{result[4].replace("http://www.sydney.edu.au/ont/", "")}</p>
-                        <p className="result-score">Score: {formatter.format(result[1])}</p>
-                        <p className="result-desc">{result[5]}</p>
+                        <p className="result-class">{result[4].replace("http://www.sydney.edu.au/ont/", "")}</p>
+                        <p className="result-summary">{result[5]}</p>
                         <a href={result[5]} className="result-link" target="_blank" rel="noreferrer">{result[6]}</a>
                     </li>
                 )}
